a115d593571f5d3411f8007de9e7e2041a5fae64
Farmertree8 <hanstwcq@gmail.com>
2025-05-04 18:19:16 +0800

    Quartz sync: May 4, 2025, 6:19 PM

--- 同步Quartz文件到專案.bat
+++ 同步Quartz文件到專案.bat
@@ -1,24 +1,5 @@
 @echo off
 setlocal
 
-:: 提示輸入 commit 訊息
-set /p COMMIT_MSG=Input commit MSG (eg. remove bad grammar):
-
-:: 建置 Quartz
 echo Running quartz sync...
 npx quartz sync
-
-:: Git 操作
-echo Changes taking effects...
-git add .
-
-echo Sending request to Git...
-git commit -m "%COMMIT_MSG%"
-
-echo Pushing changes...
-git push
-
-echo.
-echo ✅Done!
-pause
-
---
+++ 同步Quartz文件到雲端.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+
+:: 提示輸入 commit 訊息
+set /p COMMIT_MSG=Input commit MSG (eg. remove bad grammar):
+
+:: Git 操作
+echo Changes taking effects...
+git add .
+
+echo Sending request to Git...
+git commit -m "%COMMIT_MSG%"
+
+echo Pushing changes...
+git push
+
+echo.
+echo ✅Done!
+pause
+
